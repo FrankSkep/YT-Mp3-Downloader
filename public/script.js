f60d4b4de50f1d76b0984c10edff5b8531d6837d
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-06-01 12:10:38 -0700

    style

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let videoTitle; // Variable para almacenar el título del video
+let videoTitle;
 
 document.getElementById("url").addEventListener("input", function (e) {
     const url = e.target.value;
@@ -17,13 +17,13 @@ document.getElementById("url").addEventListener("input", function (e) {
             return response.json();
         })
         .then((data) => {
-            videoTitle = data.title; // Almacenar el título del video
+            videoTitle = data.title;
             const videoInfoDiv = document.getElementById("video-info");
             videoInfoDiv.innerHTML = `
-                <img src="${data.thumbnail}" alt="Thumbnail" style="width: 100px; height: auto;" />
+                <img src="${data.thumbnail}" alt="Thumbnail">
                 <p>${data.title}</p>
             `;
-            videoInfoDiv.style.display = "block";
+            videoInfoDiv.style.display = "flex";
         })
         .catch((err) => {
             console.error("Error:", err);
@@ -38,7 +38,7 @@ document.getElementById("download-form").addEventListener("submit", function (e)
     const url = document.getElementById("url").value;
     const bitrate = document.getElementById("bitrate").value;
     const loader = document.getElementById("loader");
-    loader.style.display = "block"; // Mostrar el loader
+    loader.style.display = "block";
 
     fetch("/routes/mp3-download", {
         method: "POST",
@@ -58,15 +58,15 @@ document.getElementById("download-form").addEventListener("submit", function (e)
         const a = document.createElement("a");
         a.style.display = "none";
         a.href = url;
-        a.download = `${videoTitle}.mp3`; // Utilizar el título del video como nombre del archivo
+        a.download = `${videoTitle}.mp3`;
         document.body.appendChild(a);
         a.click();
         window.URL.revokeObjectURL(url);
-        loader.style.display = "none"; // Ocultar el loader
+        loader.style.display = "none";
     })
     .catch((err) => {
         console.error("Error:", err);
         alert("Error al descargar el archivo. Por favor, intenta de nuevo.");
-        loader.style.display = "none"; // Ocultar el loader en caso de error
+        loader.style.display = "none";
     });
 });
